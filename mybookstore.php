16daefcde24c812f6f624da800f56bcad5b8afbc
Eli Kaustinen <113506242+Busters04@users.noreply.github.com>
2023-03-18 14:41:13 -0700

    changed code for mybookstore

--- mybookstore.php
+++ mybookstore.php
@@ -1,24 +1,45 @@
+<?php
+    $connection = mysqli_connect("localhost", "root", "X1*aEW17lmtq%,**", "mybookstore");
+    if(!$connection){
+        die("could not connect".mysql_connect_error());
+    }
+
+    $sql = "SELECT author_id, author_fname, author_lname, author_country FROM author_info WHERE author_id > 5";
+
+    $result = mysqli_query($connection, $sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <title>My Book Store</title>
 </head>
 <body>
+    <table align="center" border="1px" style="width:600px; line-height:40px;">
+        <tr>
+            <th colspan="4"><h2>Authors</h2></th>
+        </tr>
+        <t>
+            <th> ID </th>
+            <th> First Name </th>
+            <th> Last Name </th>
+            <th> Country </th>
+        </t>
+
     <?php 
-        $connection = mysqli_connect("localhost", "root", "X1*aEW17lmtq%,**", "mybookstore");
-        if(!$connection){
-            die("could not connect".mysql_connect_error());
-        }
-
-        $query = "select * from author_info";
-
-        $stmt = mysqli_query($connection,$query);
-
-        while($row = mysqli_fetch_array($stmt,MYSQLI_ASSOC)){
-            echo $row['author_fname'].'</br>';
+        while($rows = mysqli_fetch_assoc($result)){
+    ?>
+        <tr>
+            <td><?php echo $rows['author_id'].' '; ?></td>
+            <td><?php echo $rows['author_fname'].' '; ?></td>
+            <td><?php echo $rows['author_lname'].' '; ?></td>
+            <td><?php echo $rows['author_country'].'<br>'; ?></td>
+        </tr>
+    <?php    
         }
      ?>
+     </table>
 </body>
 </html>
